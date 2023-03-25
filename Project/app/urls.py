19f7eff1f14bfc4ee07388587e68ce6ab7ddcd86
Akshat verma <akshatvermanike@gmail.com>
2023-03-26 02:44:08 +0530

    Django AppCCompleted

--- Project/app/urls.py
+++ Project/app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 
 urlpatterns = [
-   path('',views.home, name = 'home'),
-   path('add',views.add, name='add')
+   # path('',views.home, name = "home"),
+   path('add',views.add, name='add'),
+   #path("about/",views.about, name="about"),
 ]
